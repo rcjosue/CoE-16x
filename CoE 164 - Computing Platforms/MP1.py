3fd10606951b6a693b9e534ba2347720bad8355d
rcjosue <69629086+rcjosue@users.noreply.github.com>
2021-05-15 16:18:49 +0800

    Added alpha computation
    created alpha function to calculate all alpha values to be used

--- CoE 164 - Computing Platforms/MP1.py
+++ CoE 164 - Computing Platforms/MP1.py
@@ -1,12 +1,51 @@
-T = int(input('Input number of messages: '))
-if ( (T > 10) or (T<0) )(:
-     print("Error: too many messages")
-else:
-     for num in range(T):
-          params = ( input().split(" ") )
-          if ( (params[0] != '1') or (params[0] != '2') or (params[1] != 'L') or (params[1] != 'M') or (params[1] != 'Q') or (params[1] != 'H') or (params[2] > '34') or (params[2] < '1'):
-               print ('Error: invalid parameters')
-          else:
-               bytestring = ( input().split(" ") )
+def main():
+     alphas = alpha(2)
+     print(alphas)
+     validM = {'L', 'M', 'Q', 'H'}
+     try:
+          T = int(input('Input number of messages: '))
+     except:
+          print("Error: Invalid value")          
+     if ( (T > 10) or (T<0) ):
+          print("Error: too many messages")
+     else:
+          for num in range(T):
+               params = ( input().split(" ") )
                print(params)
-               print(bytestring)
+               if ( (( params[0] != '1') and (params[0] != '2')) or (params[1] not in validM) or ( (params[2] > '34') and (params[2] < '1') ) ):
+                    print ('Error: invalid parameters')
+               else:
+                    bytestring = ( input().split(" ") )
+                    print(bytestring)
+
+def alpha(a):
+     '''Takes an integer 'a' and generates 'a' number of alpha values'''
+     alphas = []
+     for num in range (8):
+          x = pow(a,num)
+          alphas.append(x)
+     for num in range (8,256):
+          x *=a
+          if ( x > 255):
+               x ^= 285
+          alphas.append(x)
+     return alphas
+
+if __name__ == "__main__":
+     main()
+
+
+'''
+Short Journal Draft
+
+The program was written in Python 3.9.
+If else statements were used to handle errors in value and the try except was used to handle type errors
+
+Alpha computation
+for loop from 0 to 7 then 8 onwards as the pattern changes from taking alpha raised to a number into the previous alpha times alpha
+if the current value if higher than 255 it takes the value of itself XOR 285. This calculates all the values of alpha and stores
+into an array returned to the main function
+
+
+
+'''
