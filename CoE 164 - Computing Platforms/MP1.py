b15a8b9bde7dfbce496f55962483b6caf9641a04
rcjosue <69629086+rcjosue@users.noreply.github.com>
2021-05-16 10:52:49 +0800

    Created 'generator polynomial' function

--- CoE 164 - Computing Platforms/MP1.py
+++ CoE 164 - Computing Platforms/MP1.py
@@ -1,6 +1,11 @@
+import time
+
 def main():
+     global alphas
      alphas = alpha(2)
-     print(alphas)
+
+     poly = generator(4)
+     print (poly)
      validM = {'L', 'M', 'Q', 'H'}
      try:
           T = int(input('Input number of messages: '))
@@ -31,6 +36,21 @@ def alpha(a):
           alphas.append(x)
      return alphas
 
+def generator(c):
+     G = [0]
+     for i in range(1 , c):
+          G.append(0)
+          Gb = [ (x+i) % 255 for x in G]
+          for j in range( -1, -len(Gb), -1 ):
+               G[j] = alphas.index( alphas[ Gb[j] ] ^ alphas[ G[j-1] ] )
+          G[0] = Gb[0]
+     G.append(0)
+     return G
+          
+def message(N):
+     print('hi')
+          
+                 
 if __name__ == "__main__":
      main()
 
@@ -45,7 +65,7 @@ Alpha computation
 for loop from 0 to 7 then 8 onwards as the pattern changes from taking alpha raised to a number into the previous alpha times alpha
 if the current value if higher than 255 it takes the value of itself XOR 285. This calculates all the values of alpha and stores
 into an array returned to the main function
-
+pow was shown to be faster than **
 
 
 '''
