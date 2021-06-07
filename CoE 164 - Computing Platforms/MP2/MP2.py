7b0e231a6bb2e8b91e776e4e1e96e79c0237c121
rcjosue <69629086+rcjosue@users.noreply.github.com>
2021-06-08 04:13:43 +0800

    Added comments and test cases

--- CoE 164 - Computing Platforms/MP2/MP2.py
+++ CoE 164 - Computing Platforms/MP2/MP2.py
@@ -1,12 +1,14 @@
+#Author             : Ryan Izach C. Josue
+#SN                 : 2018-12231 
+#Acknowledged by    : Ivan Gabriel P. Etorma
+#SN                 : 2018-10655
+#Date               : June 8, 2021
+
 import cmath
 
-powers_of_two = [1]
-x = 1
-for num in range (1,15):
-     x *= 2
-     powers_of_two.append(x)
-   
 def fft (signal):
+     '''Recursive function for solving split radix fast fourier transform'''
+     #Discrete fourier transform of base cases where signal length is one or two
      if len(signal) == 2:
           x = ( signal[0] )
           y = ( signal[1] )
@@ -15,21 +17,22 @@ def fft (signal):
      if len(signal) == 1:
           return( signal )
      
-     fft_even = fft(signal[0::2])
-     fft_odd1 = fft(signal[1::4])
-     fft_odd2 = fft(signal[3::4])
+     #Recursive splitting of arrays
+     fft_even = fft(signal[0::2]) #array e
+     fft_odd1 = fft(signal[1::4]) #array a
+     fft_odd2 = fft(signal[3::4]) #array b
 
-     twiddling_factor = [ cmath.exp((-2j*cmath.pi*i)/ len (signal) ) for i in range( len (signal) ) ]
+     #Precomputation of Twiddling factors w_{N} with N = signal length and k 
+     twiddling_factor = [ cmath.exp((-2j*cmath.pi*k)/ len (signal) ) for k in range( len (signal) ) ]
 
-     sum_odd = [(twiddling_factor[i]*fft_odd1[i] + twiddling_factor[i*3]*fft_odd2[i]) for i in range( len(fft_odd1) ) ]
+     #Precomputation of quarters (sum and difference of odd parts of the split) using w_{N}(1, k) * a +- w_{N}(1, 3k) * b
+     sum_odd = [(twiddling_factor[i]*fft_odd1[i] + twiddling_factor[i*3]*fft_odd2[i]) for i in range( len(fft_odd1) ) ] #List comprehension for computation of 
      diff_odd = [(twiddling_factor[i]*fft_odd1[i] - twiddling_factor[i*3]*fft_odd2[i]) for i in range( len(fft_odd1) ) ]
 
-     X0 = []
-     X1 = []
-     X2 = []
-     X3 = []
+     X0,X1,X2,X3 = [],[],[],[]
 
      for i in range( len(sum_odd) ):
+          '''Computation of DFT quarters'''
           X0.append(fft_even[i] + sum_odd[i])
           X1.append(fft_even[i + len(signal)//4] - 1j*diff_odd[i])
           X2.append(fft_even[i] - sum_odd[i])
@@ -37,6 +40,13 @@ def fft (signal):
 
      return( X0 + X1 + X2 + X3 )
 
+powers_of_two = [1]
+x = 1
+for num in range (1,15):
+     '''Create an array for the powers of 2 from 1 to 14'''
+     x *= 2
+     powers_of_two.append(x)
+   
 T = int(input())
 print( T )
 for t in range(T):
@@ -45,16 +55,20 @@ for t in range(T):
      length = N
 
      for exp in powers_of_two:
+          '''Gets the closest power of 2 that is equal or greater than N'''
           if length <= exp:
                length = exp
                break
           
-     for i in range(exp-N):
+     for i in range(length-N):
+          '''Appends zeroes to resize signal to a length equal to a power of two'''
           signal.append(0)
 
-     transformed = fft(signal)
+     transformed = fft(signal) 
      output = ''
+
      for i in range(length):
+          '''Formats output of complex numbers from the list containing the transformed signal'''
           if (transformed[i].real)>=0:
                 output +='+'
           output += str(round(transformed[i].real, 6)) 
